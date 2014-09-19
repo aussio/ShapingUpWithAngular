6817ebc24258bc3022038b036118553b3f59be5c
Auss <auss@wpengine.com>
2014-09-19 14:03:44 -0500

    Got set up with CodeKit, and have completed section one of the CodeSchool course.

---
+++ app.js
@@ -0,0 +1,7 @@
+var app = angular.module('store', [ ]);
+
+var gem = {
+	name: 'Dodecahedron',
+	price: 2.95,
+	description: 'Some gems have hidden quality behind their luster, beyond their shine. Dodecahedron is one of those gems.'
+};
---
+++ min/app-min.js
@@ -0,0 +1,1 @@
+var app=angular.module("store",[]),gem={name:"Dodecahedron",price:2.95,description:"Some gems have hidden quality behind their luster, beyond their shine. Dodecahedron is one of those gems."};
