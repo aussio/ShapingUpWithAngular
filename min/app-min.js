588a8839f0deb76f36f8a47569b4d8e88fe4ee27
Auss <auss@wpengine.com>
2014-09-19 21:23:50 -0500

    removed extra min/ folder in root

--- min/app-min.js
+++
@@ -1,1 +0,0 @@
-!function(){var e=angular.module("store",[]);e.controller("StoreController",function(){this.product=o});var o={name:"Dodecahedron",price:2.95,description:"Some gems have hidden quality behind their luster, beyond their shine. Dodecahedron is one of those gems."}}();
