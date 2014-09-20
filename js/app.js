8cab972348101fc95842a06ae03db2f298f764b6
Auss <auss@wpengine.com>
2014-09-19 21:19:51 -0500

    Added in angular app. Made product.price in a label.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
+(function(){
+// Define Angular Module
 var app = angular.module('store', [ ]);
 
+// Define Angular Controller
+app.controller('StoreController', function(){
+	this.product = gem;
+});
+
+// Called by controller.product ^
 var gem = {
 	name: 'Dodecahedron',
 	price: 2.95,
 	description: 'Some gems have hidden quality behind their luster, beyond their shine. Dodecahedron is one of those gems.'
 };
+
+})();
